b6fd6d4c56ca045a8777e807ed2f3dc6b6af14c1
Uday Singh <udaysingh8790@gmail.com>
2026-06-26 17:37:19 +0530

    Time: 0 ms (100%), Space: 12 MB (40.19%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -6,7 +6,7 @@ public:
         sort(strs.begin(),strs.end());
         string f = strs[0];
         string l = strs[n-1];
-        for(int i = 0;i<f.size();i++){
+        for(int i = 0;i<min(f.size(),l.size());i++){
             if(f[i] == l[i]){
                 s += f[i];
             }else{
