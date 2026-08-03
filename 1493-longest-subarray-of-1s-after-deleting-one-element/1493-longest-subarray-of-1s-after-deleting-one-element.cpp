cab34eda2c64419a9c96fb9e2aa7a536cd2c0487
Uday Singh <udaysingh8790@gmail.com>
2026-08-03 10:01:58 +0530

    Time: 0 ms (100%), Space: 60 MB (98.25%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -3,27 +3,37 @@ public:
     int longestSubarray(vector<int>& nums) {
        int n = nums.size(); 
        int del = 0;
-       int i = 0;
-       int j = 0;
        int len = 0;
-       while(j<n){
-        if(nums[j] == 1){
-            j++;
-        }else{
-            if(!del){
-                j++;
-                del = 1;
-            }else{
-                len = max(len,j-i-1);
-                while(nums[i] != 0){
-                    i++;
-                }
-                i++;
-                del--;
-            }
+       int i = 0;
+       for(int j  = 0;j<n;j++){
+        if(nums[j] == 0) del++;
+        while(del > 1){
+            if(nums[i] == 0) del--;
+            i++;
         }
+        len = max(len,j-i);
        }
-        len = max(len,j-i-1);
        return len;
+    //    int i = 0;
+    //    int j = 0;
+    //    while(j<n){
+    //     if(nums[j] == 1){
+    //         j++;
+    //     }else{
+    //         if(!del){
+    //             j++;
+    //             del = 1;
+    //         }else{
+    //             len = max(len,j-i-1);
+    //             while(nums[i] != 0){
+    //                 i++;
+    //             }
+    //             i++;
+    //             del--;
+    //         }
+    //     }
+    //    }
+    //     len = max(len,j-i-1);
+    //    return len;
     }
 };
