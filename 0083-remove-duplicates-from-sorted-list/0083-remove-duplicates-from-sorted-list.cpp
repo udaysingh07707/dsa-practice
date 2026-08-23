496ba991b576fcf35f9355b825fdb37ec4c95fec
Uday Singh <udaysingh8790@gmail.com>
2026-08-23 19:21:32 +0530

    Time: 0 ms (100%), Space: 16.1 MB (91.9%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp = head;
+        while(temp!=nullptr && temp->next!=nullptr){
+            if(temp->next->val ==  temp->val){
+                temp->next = temp->next->next;
+            }
+            if(temp->next!=nullptr && temp->next->val!=temp->val){
+                temp = temp->next;
+            }
+        }
+        return head;
+    }
+};
