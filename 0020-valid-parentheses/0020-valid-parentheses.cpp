0ae02245ffa3d65ed2ad739585a07aa7268ef8a1
Uday Singh <udaysingh8790@gmail.com>
2026-09-03 20:56:05 +0530

    Time: 0 ms (100%), Space: 10.2 MB (6.29%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(int i = 0;i<s.length();i++){
+            if(s[i] == '(' || s[i] == '['|| s[i] == '{' ){
+                st.push(s[i]);
+            }else{
+                if(st.empty()){
+                    return false;
+                }
+                if(   s[i] == ')'   && st.top() == '(' ||
+                     s[i] == ']' &&  st.top() == '['  ||
+                     s[i] == '}' && st.top() == '{'
+                  ){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        
+        }
+        if(st.empty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
