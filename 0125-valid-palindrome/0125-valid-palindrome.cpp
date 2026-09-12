806f3333b964406d102a3c4f698d20bc3d4973d5
Uday Singh <udaysingh8790@gmail.com>
2026-09-13 00:01:25 +0530

    Time: 3 ms (27.93%), Space: 9.9 MB (76.98%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            if(!isalnum(s[i])) i++;
+            else if(!isalnum(s[j])) j--;
+            else if(toupper(s[i])!=toupper(s[j])) return false;
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+};
