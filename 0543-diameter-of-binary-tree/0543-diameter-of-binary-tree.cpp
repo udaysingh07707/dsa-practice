34350740617f340e26b51311c23002496f980f37
Uday Singh <udaysingh8790@gmail.com>
2026-09-24 00:07:52 +0530

    Time: 4 ms (20.22%), Space: 23.7 MB (41.2%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -5,20 +5,29 @@ right;
  */
 class Solution {
 public:
-    int lvl(TreeNode* root){
-        if(root == NULL) return 0;
-        return 1 + max(lvl(root->left) , lvl(root->right));
+    // int lvl(TreeNode* root){
+    //     if(root == NULL) return 0;
+    //     return 1 + max(lvl(root->left) , lvl(root->right));
+    // }
+    // void helper(TreeNode* root,int &dia){
+    //     if(root == NULL) return;
+    //     int d = lvl(root->left) + lvl(root->right);
+    //     dia = max(dia,d);
+    //     helper(root->left,dia);
+    //     helper(root->right,dia);
+    // }
+    int depth(TreeNode* root,int &dia){
+        if(root==NULL) return 0;
+        int leftdepth = depth(root->left,dia);
+        int rightdepth = depth(root->right,dia);
+        dia = max(dia,leftdepth+rightdepth);
+        return 1+max(leftdepth,rightdepth);
     }
-    void helper(TreeNode* root,int &dia){
-        if(root == NULL) return;
-        int d = lvl(root->left) + lvl(root->right);
-        dia = max(dia,d);
-        helper(root->left,dia);
-        helper(root->right,dia);
-    }
+
     int diameterOfBinaryTree(TreeNode* root) {
         int dia = 0;
-        helper(root,dia);
+        // helper(root,dia);
+        depth(root,dia);
         return dia;
 
     }
